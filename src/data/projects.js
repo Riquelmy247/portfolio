1ebc2a3ea1b363afeda172c8786acaeef47236fd
Riquelmy Martins <72394084+Riquelmy247@users.noreply.github.com>
2026-02-18 11:14:38 -0300

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -53,6 +53,56 @@ export const projects = [
   },
   {
     id: 6,
+    name: 'Lash Designer - Site Institucional para Lash Designer',
+    description: 'Site institucional para Lash Designer.',
+    technologies: ['React', 'Vite'],
+    github: [
+      { label: 'Código', url: 'https://github.com/Riquelmy247/lash-designer' },
+    ],
+    demoUrl: 'https://riquelmy247.github.io/lash-designer/',
+  },
+  {
+    id: 7,
+    name: 'SolDance - Site Institucional para SolDance',
+    description: 'Site institucional para SolDance.',
+    technologies: ['React', 'Vite'],
+    github: [
+      { label: 'Código', url: 'https://github.com/Riquelmy247/soldance' },
+    ],
+    demoUrl: 'https://riquelmy247.github.io/soldance/',
+  },
+  {
+    id: 8,
+    name: 'Biocorpore - Site Institucional para Biocorpore',
+    description: 'Site institucional para Biocorpore.',
+    technologies: ['React', 'Vite'],
+    github: [
+      { label: 'Código', url: 'https://github.com/Riquelmy247/biocorpore' },
+    ],
+    demoUrl: 'https://riquelmy247.github.io/biocorpore/',
+  },
+  {
+    id: 9,
+    name: 'Impaccto - Site Institucional para Impaccto',
+    description: 'Site institucional para Impaccto.',
+    technologies: ['React', 'Vite'],
+    github: [
+      { label: 'Código', url: 'https://github.com/Riquelmy247/impaccto' },
+    ],
+    demoUrl: 'https://riquelmy247.github.io/impaccto/',
+  },
+  {
+    id: 10,
+    name: 'Lista de Presença para Conferência',
+    description: 'Sistema para controle de presença em conferências.',
+    technologies: ['Java', 'HTML', 'CSS', 'Spring Boot'],
+    github: [
+      { label: 'Back-end', url: 'https://github.com/Riquelmy247/lista-conferencia' },
+    ],
+    demoUrl: null,
+  },
+  {
+    id: 11,
     name: 'Cadastro Dotnet',
     description: 'Sistema simples para cadastro de usuários, utilizando C# e ASP.NET Core.',
     technologies: ['C#', 'ASP.NET', 'HTML', 'CSS'],
@@ -62,7 +112,7 @@ export const projects = [
     demoUrl: null,
   },
   {
-    id: 7,
+    id: 12,
     name: 'Universidade Cadastro',
     description: 'Sistema para cadastro de alunos, professores e cursos em uma universidade.',
     technologies: ['Java', 'Spring Boot'],
@@ -72,7 +122,7 @@ export const projects = [
     demoUrl: null,
   },
   {
-    id: 8,
+    id: 13,
     name: 'Projetos Simples em Java',
     description:
       'Projeto de jogos simples desenvolvidos em Java, incluindo um jogo de adivinhação e um jogo de labirinto.',
@@ -83,7 +133,7 @@ export const projects = [
     demoUrl: null,
   },
   {
-    id: 9,
+    id: 14,
     name: 'Projetos Simples em Python',
     description:
       'Projeto de jogos simples desenvolvidos em Python, incluindo um jogo de adivinhação e um jogo de labirinto.',
