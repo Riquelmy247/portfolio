13d9cd7bf175b319fb8b56087841d2fdadd9b1d9
Riquelmy Martins <72394084+Riquelmy247@users.noreply.github.com>
2026-02-20 21:20:22 -0300

    ajuste

--- src/data/profile.js
+++ src/data/profile.js
@@ -8,5 +8,5 @@ Minha trajetória inclui desenvolvimento para empresas de diversos segmentos, ig
 
 Trabalho com foco em qualidade, prazos e comunicação clara. Se você precisa de um site, sistema administrativo, agendamento ou solução personalizada, posso ajudar.`,
   whatsappNumber: '5534992308318',
-  avatar: './src/img/foto.jpg',
+  avatar: './logo.png',
 }
